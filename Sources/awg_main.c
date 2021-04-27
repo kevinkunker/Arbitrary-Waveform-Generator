2547a89c7c14da568e788c30ae9d166892b492ed
kevinkunker <kevinkunker@gmail.com>
2021-04-27 15:41:58 -0400

    Add sine and triangle data

--- Sources/awg_main.c
+++ Sources/awg_main.c
@@ -5,4 +5,12 @@
  *      Author: Owner
  */
 
+#include <stdint.h>
 
+const uint32_t sine_data[] = {
+#include "sine_data_100_points.txt"
+};
+
+const uint32_t triangle_data[] = {
+#include "triangle_data_100_points.txt"
+};
