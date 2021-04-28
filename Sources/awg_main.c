24cd168e7f7a0190d454d11909d6010a44eb9945
kevinkunker <kevinkunker@gmail.com>
2021-04-28 10:30:03 -0400

    Add initialization code

--- Sources/awg_main.c
+++ Sources/awg_main.c
@@ -5,7 +5,22 @@
  *      Author: Owner
  */
 
+#include <stdlib.h>
+#include <stdio.h>
 #include <stdint.h>
+#include "derivative.h"
+#include "OpenSDA_UART.h"
+#include "mcg.h"
+#include "KL25Z_port.h"
+#include "analog.h"
+#include "KL25Z_NVIC.h"
+#include "KL25Z_pit.h"
+
+/* FreeRTOS */
+#include "FreeRTOS.h"
+#include "timers.h"
+#include "semphr.h"
+#include "stream_buffer.h"
 
 const uint32_t sine_data[] = {
 #include "sine_data_100_points.txt"
@@ -14,3 +29,43 @@ const uint32_t sine_data[] = {
 const uint32_t triangle_data[] = {
 #include "triangle_data_100_points.txt"
 };
+
+int main(void){
+	/************************************************************
+	 * Set the main clock to 48 MHz 
+	 ************************************************************/
+	pll_init(8000000,0,1,4,24,1);
+	
+	/************************************************************
+	 * Enable peripheral clocks 
+	 ************************************************************/
+	enable_port_clock(PORTA_PERIPHERAL);
+	enable_port_clock(PORTB_PERIPHERAL);
+	enable_port_clock(PORTC_PERIPHERAL);
+	enable_port_clock(PORTD_PERIPHERAL);
+	enable_port_clock(PORTE_PERIPHERAL);
+	enable_opensda_uart_clock();
+	enable_pit_clock();
+	enable_dac_clock();
+
+	/************************************************************
+	 *  Startup the OpenSDA UART (UART0)
+	 ************************************************************/
+	init_opensda_uart_pins();
+	init_opensda_uart(OPENSDA_UART_BAUD_CLOCK_MCGPLLCLKDIV2, OPENSDA_UART_BAUD_115200,
+    				OPENSDA_UART_STOP_BITS_1, OPENSDA_UART_PARITY_OFF, OPENSDA_UART_PARITY_EVEN);
+	
+	/* send out a welcome message */
+	opensda_uart_transmit_string("ELT3050 FreeRTOS ADC Experiments \r\n");
+
+	/* UART0 IRQ settings */
+	NVIC_ClearPendingIRQ(UART0_IRQn);
+	NVIC_SetPriority(UART0_IRQn, UART0_IRQ_PRIORITY);
+	NVIC_EnableIRQ(UART0_IRQn);
+
+	/************************************************************
+	 * startup the DAC, ADC and hardware timer
+	 ************************************************************/
+	init_dac();
+	init_dac_pin();
+}
